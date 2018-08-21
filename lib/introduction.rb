fd7abb4b5f3d9eb9ca92c0cfe3935b15339adcc7
John Martinez <jmartinez729@gmail.com>
2018-08-21 18:46:59 +0000

    automatically backed up by learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -1,7 +1,6 @@
-def introduction(name)
+def introduction(name, language)
   puts "Hi, my name is #{name}."
-end 
-
-def introduction_with_language(name, language)
+  
   puts "Hi, my name is #{name} and I'm learning to program in #{language}."
 end 
+
